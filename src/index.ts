54e247060ad0abce0d7f43b2721491698e2c44b4
duongmh <maihaiduong2015@gmail.com>
2023-08-11 11:05:09 +0700

    test-commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as dotenv from 'dotenv'
 import app from './server'
-
+import express from 'express'
 dotenv.config()
-
-const port=50002
-app.listen(port,()=>{
-    console.log(`server on http://localhost:${port}`)
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`server is running !`)
 })
