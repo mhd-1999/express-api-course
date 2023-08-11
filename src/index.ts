e8a288d0c77ccf0bfe6055199163d256b501a860
duongmh <maihaiduong2015@gmail.com>
2023-08-11 11:06:18 +0700

    test commit

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from './server'
 import express from 'express'
 dotenv.config()
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`server is running !`)
+    console.log(`server is running`)
 })
